f32c46b361cee2c48656ed4b1a85e881c2dedda1
miumiu0917 <miumiu917@gmail.com>
2017-11-13 07:46:43 +0000

    データセット作成

---
+++ create_dataset.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy.random import normal
+from pandas import DataFrame
+
+NUM_DATASET = 1000
+DATA_LEN = 1000
+
+def main():
+  data_x = np.linspace(0, 10, DATA_LEN)
+  l = []
+  for i in range(NUM_DATASET):
+    l.append(np.sin(2.0 * np.pi * data_x) + normal(loc=0.0, scale=0.3, size=DATA_LEN))
+  df = DataFrame(np.array(l))
+  train = df[0:int(NUM_DATASET*0.8)]
+  test = df[int(NUM_DATASET*0.8):NUM_DATASET]
+  train.T.to_csv('data/train_data.csv', index=False, header=False)
+  test.T.to_csv('data/test_data.csv', index=False, header=False)
+
+if __name__ == '__main__':
+  main()
--- model.py
+++ model.py
@@ -5,14 +5,14 @@ class Seq2seq(object):
   
   def __init__(self, sess):
     self.sess = sess
-    self._build_model()
+    self._build_model(1)
 
 
 
   def _build_model(self, encoder_inputs):
    
     embedding_encoder = tf.variable_scope.get_variable(
-        "embedding_encoder", [src_vocab_size, embedding_size]
+        "embedding_encoder", [src_vocab_size, embedding_size])
     encoder_emb_inp = tf.embedding_ops.embedding_lookup(
         embedding_encoder, encoder_inputs)
 
