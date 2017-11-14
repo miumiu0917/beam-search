01f89697511fd63f425774e9cc9b134b6c4b49c5
miumiu0917 <miumiu917@gmail.com>
2017-11-14 04:40:26 +0000

    encoderの実装

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ import tensorflow as tf
 from model import Seq2seq
 
 
+tf.app.flags.DEFINE_integer("encoder_vocab_size", 50000,
+                          "Encoder vocabrary size.")
+tf.app.flags.DEFINE_integer("size", 256,
+                          "Size of each model layer.")
+tf.app.flags.DEFINE_integer("max_input_length", 200,
+                          "Length of max input vector.")
+tf.app.flags.DEFINE_integer("batch_size", 64,
+                          "Size of training data batch.")
+FLAGS = tf.app.flags.FLAGS
+
 def main():
-  seq2seq = Seq2seq(tf.Session())  
+  seq2seq = Seq2seq(tf.Session(),
+    FLAGS.encoder_vocab_size,
+    FLAGS.size,
+    FLAGS.max_input_length,
+    FLAGS.batch_size
+  )  
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -3,19 +3,25 @@ import tensorflow as tf
 
 class Seq2seq(object):
   
-  def __init__(self, sess):
+  def __init__(self, sess, encoder_vocab_size,
+  size, max_input_length, batch_size):
     self.sess = sess
-    self._build_model(1)
+    self.encoder_vocab_size = encoder_vocab_size
+    self.size = size
+    self.max_input_length = max_input_length
+    self.batch_size = batch_size
+    self._build_model()
 
 
 
-  def _build_model(self, encoder_inputs):
-   
-    embedding_encoder = tf.variable_scope.get_variable(
-        "embedding_encoder", [src_vocab_size, embedding_size])
-    encoder_emb_inp = tf.embedding_ops.embedding_lookup(
-        embedding_encoder, encoder_inputs)
+  def _build_model(self):
+    encoder_inputs = tf.placeholder(tf.int32, shape=[self.max_input_length, None], name="encoder_input")
+    embedding_encoder = tf.get_variable("embedding_encoder", [self.encoder_vocab_size, self.size])
+    encoder_emb_inp = tf.nn.embedding_lookup(embedding_encoder, encoder_inputs)
+    encoder_cell = tf.nn.rnn_cell.BasicLSTMCell(256)
 
+    encoder_outputs, encoder_state = tf.nn.dynamic_rnn(encoder_cell, encoder_emb_inp, 
+      dtype=tf.float32)
   
   def train(self):
     pass
